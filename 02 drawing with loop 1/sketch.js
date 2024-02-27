d9bc685ac2bd1add1172bb10697be844a242b953
nonWuname <155467414+nonWuname@users.noreply.github.com>
2024-02-27 15:24:45 -0600

    Update sketch.js

--- 02 drawing with loop 1/sketch.js
+++ 02 drawing with loop 1/sketch.js
@@ -1,15 +1,49 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+  // drawing with loop 1
+  // Luke Wu
+  // February 27, 2024
+  //
+  // Extra for Experts:
+  // use loop to create + Array to create some visulization
 
+  let point1, point2;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+  function setup() {
+    createCanvas(400, 300);
+    point1 = [width*0.05, width * 0.05 , width *0.95 , width * 0.95];
+    point2 = [height*0.05, height*0.95,height*0.05 , height*0.95];
+  }
 
-function draw() {
-  background(220);
-}
+  function mousePressed(){
+    point1.push(mouseX);
+    point2.push(mouseY);
+  }
+
+  function cornerAndMouse(){
+    // draw same circles near each the four corners
+    // and connect some line to mouse position
+    
+    fill(255);
+    
+
+    
+    for(let i = 0; i < point1.length; ++i){
+      circle(point1[i],point2[i],20);
+    }
+    
+    for(let i = 0; i < point1.length; ++i){
+      line(point1[i], point2[i], mouseX, mouseY);
+    }
+
+  }
+
+  function draw() {
+    background(220);
+
+    cornerAndMouse();
+
+    if(point1.length > 10 && point2.length > 10){
+      point1.length = 4;
+      point2.length = 4; 
+    }
+    
+  }
