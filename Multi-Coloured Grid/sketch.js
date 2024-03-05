ff011f6e4ab658d682dbcbe76b714bcc3c9881ec
nonWuname <155467414+nonWuname@users.noreply.github.com>
2024-03-05 15:21:41 -0600

    might be second last update

--- Multi-Coloured Grid/sketch.js
+++ Multi-Coloured Grid/sketch.js
@@ -5,41 +5,85 @@
 // I will be showing off my looping skills.  make a colour grid, not very important and hard subject
 // so i won't explain too much, just a assignment and with show some my art talent(maybe) 
 
-let squaresize = 10;
+let squaresize = 0; // the size of square
+let factor = []; // two solution to solve, by complete or cut square; i don't want waste time to come up two solution with no bonus, thus only with factor: fit entirely,
+// cut square is not consider anymore
+let factor_index = 0; // the index of factor, notice the less the index is, the less the squaersize is since i iterate from small to large
 
 function setup() {
-  createCanvas(400, 400);
+  let size = 3100;// the size var to help people don't need to plug in two number at same time
+  createCanvas(size, size);
+
+  // to ignore the right click
   document.addEventListener("contextmenu", event => event.preventDefault());
-  createGrid();
+  
+  //initial the factor list so player can know the 
+  init_factor();
+  //after init factor, init squaersize at the middle of the list
+  squaresize = factor[factor_index];
+  // obviously, createGrid
+  createGrid(); 
+
+  
+
+  
+}
+
+function init_factor()
+{
+  // this help me to init the factor of the canvans since only and only if a|width, the size of 'a' will work perfectly
+  
+  //corner case, i hope won't happen, if the size of canvans is less than 20
+  if(width <= 20){
+    factor.push(width);
+  }
+  // main case, travesal from 20--width, all the i|width will be put into the factor list
+  if(factor.length == 0){
+    for(let i = 20; i <= width ; ++i){
+      if(width % i == 0) factor.push(i);
+    }
+  }
+  // since the index is not init yet, i will do that right here
+  factor_index = Math.round(factor.length / 2);
 }
 
 function createGrid(){
-  for(let x = 0; x <=width;  x+=squaresize){
-    for(let y = 0; y <= height; y +=squaresize){
-      let r = random(0,556) , g = random(0,556), b = random(0,556);
-      fill(map(Math.round(r),0,555,0,255), map(Math.round(g),0,555,0,255), map(Math.round(b),0,555,0,255));
-
+  // this is the basic function, no need to explain too much, just use nested loop to create a grid with square and fill with colour
+  for(let x = 0; x < width ;  x+=squaresize){
+    for(let y = 0; y <= width; y +=squaresize){;
+      let r = random(0,256) , g = random(0,256), b = random(0,256);
+      stroke(random(x,y));
+      
+      strokeWeight(x%10);
+      stroke((x**2)%255,(y**2)%255,(x*y)%255);
+      fill(r,g,b);
+      
       square(x,y,squaresize);
     }
   }
 }
 
 function keyPressed() {
+  // to create grid and paint again after press key
   createGrid();
 }
 
 function mousePressed() {
-  if(mouseButton === LEFT) 
+  // do the if statement first
+  if(mouseButton == LEFT) 
   { 
-    while(true){
-
-    }
+    if(factor_index <= factor.length - 2 ) squaresize = factor[++factor_index];
   }
-  else if(mouseButton === RIGHT) 
+  else if(mouseButton == RIGHT) 
   { 
-    squaresize ++;
+    if(factor_index >= 1) squaresize = factor[--factor_index];
+    
   }  
+  //if(mouseButton != CENTER) this can avoid do nothing and fill agian when press center, however Mr.scout said it is not needed, thus put as comment
+  
+  //afrer change it, we redraw the grid
   createGrid();
   
 }
-
+// sidenote, the cut solution is possible, but inconvenience, we just need to make sure traversal at the point width - squaresize *2 if(width % y != 0) break;
+// and add bunch code, but it is not important and i am lazy since i need add more if statement
