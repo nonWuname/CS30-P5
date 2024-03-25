a1a64eecfe6359f64ac33c00ddb9e171b622e5d4
nonWuname <155467414+nonWuname@users.noreply.github.com>
2024-03-25 15:19:51 -0600

    Update sketch.js

--- 09/sketch.js
+++ 09/sketch.js
@@ -7,9 +7,27 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  stroke(0,80);
+  angleMode(DEGREES);
+  background(220);
+
+}
+
+function randomelement(currlen){
+  push();
+  rotate(random(360));
+  while(currlen > 5){
+    
+    rotate(random(-40,40));
+    line(0,0,0,currlen);
+    translate(0,currlen);
+    currlen*=0.75;
+  }
+  pop();
 }
 
 function draw() {
-  background(220);
+  translate(width/2,height/2);
+  randomelement(75);
 }
