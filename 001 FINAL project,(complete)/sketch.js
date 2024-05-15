948efe012876d10ed526c96d6613287a8f228179
nonWuname <155467414+nonWuname@users.noreply.github.com>
2024-05-15 15:38:37 -0600

    reorganize file from what i got and rename it
    i make a  folder called delete, those are the assets i won't used but i might used it if i have more time

--- 001 FINAL project,(complete)/sketch.js
+++ 001 FINAL project,(complete)/sketch.js
@@ -5,11 +5,68 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let floorset = [];
+let wallset = [];
+
+
+
+let temp;
+
+function preload(){
+  
+  loadAssets();
+
+}
+
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(24*16,18*16);
+  
 }
 
 function draw() {
   background(220);
+  image(temp,mouseX,mouseY);
+}
+
+
+function loadAssets(){
+  // a function load all game assets
+  
+  // load floor
+  for(let i =0 ; i < 8; ++i){
+    floorset.push(loadImage("assets/floor/" + i + ".png"));
+  }
+
+  // load floor spike
+  temp = loadImage("assets\\wall\\simple\\column_wall.png")
+
+}
+
+class Character{
+
+  constructor(){
+
+  }
+
+}
+
+
+class Ani{
+
+  constructor(){
+
+  }
+
+}
+
+
+
+function grid(){
+  for(let y = 0; y < height; y+=16){
+    for(let x = 0; x < width; x +=16){
+      image(floorset[int(random(floorset.length))],x,y);
+    }
+  }
 }
